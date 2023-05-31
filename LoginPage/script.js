332e0b1c0927e7781363ece6c90b00e062fbf502
Trent Robinett <tr.robinettt@gmail.com>
2023-05-31 17:43:59 -0500

    Updated Login and Register pages.
    Made it so text clears when switching between them or closing them. Also made both Email fields hover on any text not just the blank@blank.com part.

--- LoginPage/script.js
+++ LoginPage/script.js
@@ -4,27 +4,38 @@ const registerLink = document.querySelector('.register-link');
 const btnPopup = document.querySelector('.btnLogin-popup');
 const iconClose = document.querySelector('.icon-close');
 
+const loginEmailInput = document.getElementById('loginEmail');
+const registerEmailInput = document.getElementById('registerEmail');
+const loginPasswordInput = document.getElementById('loginPassword');
+const registerUsernameInput = document.getElementById('registerUsername');
+const registerPasswordInput = document.getElementById('registerPassword');
+
 const registrationForm = document.querySelector('#registrationForm'); 
 const termsAndConditionsCheckbox = document.querySelector('#termsAndConditions');
 
 registerLink.addEventListener('click', ()=>{
     wrapper.classList.add('active');
+    resetFields();
 });
 
 loginLink.addEventListener('click', ()=>{
     wrapper.classList.remove('active');
+    resetFields();
 });
 
 btnPopup.addEventListener('click', ()=>{
     wrapper.classList.toggle('active-popup');
+    resetFields();
 });
 
 iconClose.addEventListener('click', ()=>{
     wrapper.classList.remove('active-popup');
+    resetFields();
 });
 
 btnPopup.addEventListener('click', ()=>{
     wrapper.classList.remove('active');
+    resetFields();
 });
 
 registrationForm.addEventListener('submit', function(e){
@@ -34,3 +45,27 @@ registrationForm.addEventListener('submit', function(e){
         alert('You must agree to the terms and conditions before registering.'); 
     }
 });
+
+function moveLabelUpOnInput(inputElement) {
+    inputElement.addEventListener('input', function(){
+        if (inputElement.value.trim().length > 0){
+            inputElement.nextElementSibling.style.top = "-5px";
+        } else {
+            inputElement.nextElementSibling.style.top = "50%";
+        }
+    });
+}
+
+moveLabelUpOnInput(loginEmailInput);
+moveLabelUpOnInput(registerEmailInput);
+moveLabelUpOnInput(loginPasswordInput);
+moveLabelUpOnInput(registerUsernameInput);
+moveLabelUpOnInput(registerPasswordInput);
+
+function resetFields() {
+    let inputs = [loginEmailInput, registerEmailInput, loginPasswordInput, registerUsernameInput, registerPasswordInput];
+    inputs.forEach(input => {
+        input.value = '';
+        input.nextElementSibling.style.top = "50%";
+    });
+}
