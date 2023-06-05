1f7ac4da4ec6888a06bfbb3a102fdae408a92a7e
Trent Robinett <tr.robinettt@gmail.com>
2023-06-04 20:00:01 -0500

    Added localStorage capibilities.
    Made it so tasks are now saved to localStorage.

--- Vue_ToDo/app.js
+++ Vue_ToDo/app.js
@@ -1,51 +1,69 @@
 const app = Vue.createApp({
     data() {
-        return {
-            newTask: '',
-            tasks: [],
-            editIndex: null,
-            editText: '',
-            currentFilter: 'all'
-        }
+      return {
+        newTask: '',
+        tasks: [],
+        editIndex: null,
+        editText: '',
+        currentFilter: 'all'
+      };
+    },
+    created() {
+      const savedTasks = localStorage.getItem('tasks');
+      if (savedTasks) {
+        this.tasks = JSON.parse(savedTasks);
+      }
+    },
+    watch: {
+      tasks: {
+        handler() {
+          this.saveTasksToLocalStorage();
+        },
+        deep: true
+      }
     },
     methods: {
-        addTask() {
-            if (this.newTask) {
-                this.tasks.push({ text: this.newTask, done: false });
-                this.newTask = '';
-            }
-        },
-        toggleDone(index) {
-            this.tasks[index].done = !this.tasks[index].done;
-        },
-        editTask(index) {
-            this.editIndex = index;
-            this.editText = this.tasks[index].text;
-        },
-        saveEdit() {
-            this.tasks[this.editIndex].text = this.editText;
-            this.editIndex = null;
-            this.editText = '';
-        },
-        clearAllTasks() {
-            this.tasks = [];
-        },
-        filterTasks(filter) {
-            this.currentFilter = filter;
+      addTask() {
+        if (this.newTask.trim() !== '') {
+          this.tasks.push({ text: this.newTask, done: false });
+          this.newTask = '';
         }
+      },
+      toggleDone(index) {
+        this.tasks[index].done = !this.tasks[index].done;
+      },
+      editTask(index) {
+        this.editIndex = index;
+        this.editText = this.tasks[index].text;
+      },
+      saveEdit() {
+        this.tasks[this.editIndex].text = this.editText;
+        this.editIndex = null;
+        this.editText = '';
+      },
+      clearAllTasks() {
+        this.tasks = [];
+      },
+      filterTasks(filter) {
+        this.currentFilter = filter;
+      },
+      saveTasksToLocalStorage() {
+        localStorage.setItem('tasks', JSON.stringify(this.tasks));
+      }
     },
     computed: {
-        filteredTasks() {
-            if (this.currentFilter === 'all') {
-                return this.tasks;
-            } else if (this.currentFilter === 'pending') {
-                return this.tasks.filter(task => !task.done);
-            } else if (this.currentFilter === 'done') {
-                return this.tasks.filter(task => task.done);
-            }
-            return this.tasks;
+      filteredTasks() {
+        if (this.currentFilter === 'all') {
+          return this.tasks;
+        } else if (this.currentFilter === 'pending') {
+          return this.tasks.filter(task => !task.done);
+        } else if (this.currentFilter === 'done') {
+          return this.tasks.filter(task => task.done);
         }
+        return this.tasks;
+      }
     }
-});
-
-app.mount('#app');
+  });
+  
+  app.mount('#app');
+  
